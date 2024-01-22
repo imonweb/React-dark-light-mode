fee803dccdf29bab19aa6959ccbe46733a9c17bf
Imon <contact@imonweb.co.uk>
2024-01-22 21:24:27 +0000

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+ 
+import './App.css'
+import Form from './components/Form'
+
+function App() {
+  
+
+  return (
+    <div className='App' id="light">
+     <Form />
+    </div>
+  )
+}
+
+export default App
